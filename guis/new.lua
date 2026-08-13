3d71dc11f6a60870fa352abb1248dbf326fd7c1f
MostOpps <angelisrealgoat@gmail.com>
2026-08-13 14:50:45 +0900

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -3673,11 +3673,12 @@ function mainapi:CreateGUI()
 	logo.Position = UDim2.fromOffset(11, 0)
 	logo.Parent = window
 	local logov4 = Instance.new('ImageLabel')
-	logov4.Name = 'V4Logo'
+	logov4.Name = 'CustomV4Logo'
 	logov4.Size = UDim2.fromOffset(28, 16)
 	logov4.Position = UDim2.new(1, -4, 0, 1)
 	logov4.BackgroundTransparency = 1
 	logov4.Image = getcustomasset('newvape/assets/new/guiv4.png')
+	logov4.ImageColor3 = Color3.fromRGB(255, 255, 255)
 	logov4.Size = UDim2.fromOffset(46, 38)
 	logov4.Parent = logo
 	local children = Instance.new('Frame')
